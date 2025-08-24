038aa15024b90efef353811b1407cf00e4efbe26
BangAldy21 <anggachannel011@gmail.com>
2025-08-24 16:28:27 +0800

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,3 @@
-import "remixicon/fonts/remixicon.css";
 import Dock from "./Dock/Dock";
 import { VscHome, VscArchive, VscAccount } from "react-icons/vsc";
 
@@ -11,23 +10,22 @@ const Footer = () => {
 
   return (
     <div className="mt-32 pb-8 flex flex-col items-center relative z-10">
-      {/* Flex container adaptif */}
       <div className="w-full flex flex-col md:flex-row items-center md:justify-between gap-6">
         
-        {/* Judul - paling atas di mobile */}
+        {/* Judul */}
         <h1 className="text-2xl font-bold order-1 md:order-none">
           Portofolio
         </h1>
 
-        {/* Ikon Sosmed - di tengah di mobile */}
+        {/* Ikon Sosmed */}
         <div className="flex gap-3 order-2 md:order-none">
           <a href="https://github.com/rissss21"><i className="ri-github-fill ri-2x"></i></a>
           <a href="https://www.instagram.com/farisedrikprayoga/"><i className="ri-instagram-fill ri-2x"></i></a>
           <a href="https://www.youtube.com/@FarisEdrikPrayoga"><i className="ri-youtube-fill ri-2x"></i></a>
         </div>
 
-        {/* Dock - paling bawah di mobile */}
-        <div className="order-3 md:order-none mt-15 md:mt-0  md:mb-0">
+        {/* Dock */}
+        <div className="order-3 md:order-none mt-15 md:mt-0 md:mb-0">
           <Dock 
             items={items}
             panelHeight={30}
@@ -35,7 +33,6 @@ const Footer = () => {
             magnification={100}
           />
         </div>
-
       </div>
     </div>
   );
